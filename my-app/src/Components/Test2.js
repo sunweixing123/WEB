6e40b58b059dfb5f465f2bf58a0f08faa674b17c
sunweixing123 <359561000@qq.com>
2018-07-12 22:14:59 +0800

    test

---
+++ my-app/src/Components/Test2.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+class Test2 extends Component {
+    render(){
+        return (
+            <div>
+                <h1>打卡下班</h1>
+            </div>
+        )
+    }
+}
